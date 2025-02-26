3cdaa932ec71408ddf2d2798dc62b2ea9c88b5ac
bholmes-learning <bh.learning.studies@gmail.com>
2025-02-26 18:21:17 -0500

    Create locals.tf

---
+++ locals.tf
@@ -0,0 +1,8 @@
+locals{
+  resource_group_name="mcit-bholmes-rg"
+  location="Canada Central"
+  vnet_name="rgVNet"
+  address_space=["10.0.0.0/16"]
+  subnet_name="rgSubnet"
+  address_prefixes=["10.0.1.0/24"]
+}
